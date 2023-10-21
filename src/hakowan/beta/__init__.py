59e47d3ac6f447561c5c80e5cafc40d1231a7eca
Qingnan Zhou <qzhou@adobe.com>
2023-10-21 10:55:36 -0400

    Expose material module.

--- src/hakowan/beta/__init__.py
+++ src/hakowan/beta/__init__.py
@@ -3,4 +3,5 @@ from .common import color, named_colors, logger
 from .grammar import dataframe, mark, channel, scale, texture, transform, layer
 from .grammar.layer import Layer
 from .grammar.scale import Attribute
+from .grammar.channel import material
 from .render import render
