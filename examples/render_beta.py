a85941f005cc766b1b8e40098e727d282f14ce3d
Qingnan Zhou <qzhou@adobe.com>
2023-10-31 18:16:50 -0400

    Update example.

--- examples/render_beta.py
+++ examples/render_beta.py
@@ -3,7 +3,7 @@
 """ A command line rendering example using hakowan. """
 
 import argparse
-import hakowan.beta as hkw
+import hakowan as hkw
 import logging
 import lagrange
 import numpy as np
@@ -15,20 +15,10 @@ def parse_args():
     parser.add_argument("input_mesh", help="Input mesh file")
     parser.add_argument("output_image", help="Output image file")
     parser.add_argument(
-        "--euler",
-        default=[0, 0, 0],
-        nargs=3,
-        help="Transformation defined in Euler angles",
+        "-Z", "--z-up", help="Make Z axis the up direction", action="store_true"
     )
     parser.add_argument("-H", "--height", help="Output image height", default=800)
     parser.add_argument("-W", "--width", help="Output image width", default=1024)
-    parser.add_argument(
-        "-s", "--num-samples", help="Number of samples", default=64, type=int
-    )
-    parser.add_argument("-c", "--color", help="Base color", default="ivory")
-    parser.add_argument("-r", "--roughness", help="Material roughness", default=0.5)
-    parser.add_argument("-m", "--metallic", help="Material metallic", default=0.0)
-    parser.add_argument("--uv-scale", help="UV scale", default=1.0, type=float)
     return parser.parse_args()
 
 
@@ -37,63 +27,20 @@ def main():
     hkw.logger.setLevel(logging.INFO)
 
     mesh = lagrange.io.load_mesh(args.input_mesh)
-    uv_ids = mesh.get_matching_attribute_ids(usage=lagrange.AttributeUsage.UV)
-    mesh.create_attribute(
-        "x",
-        element=lagrange.AttributeElement.Vertex,
-        usage=lagrange.AttributeUsage.Scalar,
-        initial_values=mesh.vertices[:, 0].copy(),
+
+    base = (
+        hkw.layer.Layer()
+        .data(mesh)
+        .mark(hkw.mark.Surface)
+        .channel(material=hkw.material.RoughConductor(material="Al"))
     )
 
-    base = hkw.layer.Layer().data(mesh).mark(hkw.mark.Surface)
-    if len(uv_ids) > 0:
-        uv_name = mesh.get_attribute_name(uv_ids[0])
-        scalar_field = hkw.texture.ScalarField(
-            data=hkw.Attribute(name="x"), colormap="viridis"
-        )
-        checkerboard = hkw.texture.CheckerBoard(
-            uv=hkw.Attribute(name=uv_name, scale=hkw.scale.Uniform(factor=10)),
-            texture1=hkw.texture.Uniform(color=0.2),
-            texture2=hkw.texture.Uniform(color=0.8),
-        )
-        isocontour = hkw.texture.Isocontour(
-            data=hkw.Attribute(name="x", scale=hkw.scale.Uniform(factor=0.1)),
-            ratio=0.1,
-            texture1=hkw.texture.Uniform(color=0.9),
-            texture2=scalar_field,
-        )
-        diffuse = hkw.channel.Diffuse(reflectance=checkerboard)
-        conductor = hkw.channel.RoughConductor(material="Al", alpha=0.2)
-        plastic = hkw.channel.Plastic(
-            diffuse_reflectance=checkerboard,
-            specular_reflectance=1.0,
-        )
-        principled = hkw.channel.Principled(
-            color=scalar_field, roughness=scalar_field, metallic=scalar_field
-        )
-
-        base = base.channel(material=principled)
-    else:
-        base = base.channel(
-            #material=hkw.channel.Principled(
-            #    color=hkw.texture.Uniform(color=args.color),
-            #    metallic=hkw.texture.Isocontour(
-            #        data=hkw.Attribute(name="x", scale=hkw.scale.Uniform(factor=0.1)),
-            #        ratio=0.1,
-            #        texture1=hkw.texture.Uniform(color=0.9),
-            #        texture2=hkw.texture.ScalarField(
-            #            data=hkw.Attribute(name="x"), colormap="viridis"
-            #        ),
-            #    ),
-            #),
-            material=hkw.channel.RoughConductor(
-                material="W",
-            )
-        )
-
     config = hkw.config.Config()
-    config.z_up()
-    config.sampler.sample_count = args.num_samples
+    config.film.width = args.width
+    config.film.height = args.height
+    if args.z_up:
+        config.z_up()
+
     hkw.render(base, config, filename=args.output_image)
 
 
