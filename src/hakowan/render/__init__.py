70c4eec4da5b3499af24bb405c2b68da8e0b56f4
Qingnan Zhou <qzhou@adobe.com>
2023-11-25 18:38:36 -0500

    Fix linux failure.

--- src/hakowan/render/__init__.py
+++ src/hakowan/render/__init__.py
@@ -6,6 +6,9 @@ import mitsuba as mi
 if mi.variant() is None:
     for variant in ["cuda_ad_rgb", "llvm_ad_rgb", "scalar_rgb"]:
         if variant in mi.variants():
-            mi.set_variant(variant)
-            break
+            try:
+                mi.set_variant(variant)
+                break
+            except:
+                pass
     assert mi.variant() is not None
